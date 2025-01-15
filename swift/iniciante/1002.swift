e6209a21b467c00415d7a255d446150f97d24e2a
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-01-15 09:28:48 -0300

    fix variable name from 'raio' to 'radius' for clarity in area calculation

--- swift/iniciante/1002.swift
+++ swift/iniciante/1002.swift
@@ -1,9 +1,9 @@
 import Foundation
 
-if let input = readLine(), let raio = Double(input.replacingOccurrences(of: ",", with: ".")) {
+if let input = readLine(), let radius = Double(input.replacingOccurrences(of: ",", with: ".")) {
   let pi = 3.14159
 
-  let area = pi * pow(raio, 2)
+  let area = pi * pow(radius, 2)
 
   print(String(format: "A=%.4f", area))
 } else {
