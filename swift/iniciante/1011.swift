dad0019635a6cb91b60e386fb4ba49ca684522b6
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-01-15 10:00:38 -0300

    1011

---
+++ swift/iniciante/1011.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func calculateSphereVolume(radius: Double) -> Double {
+  let pi = 3.14159
+  let volume = (4.0 / 3.0) * pi * pow(radius, 3)
+  return volume
+}
+
+if let input = readLine(), let radius = Double(input.replacingOccurrences(of: ",", with: ".")) {
+  let sphereVolume = calculateSphereVolume(radius: radius)
+
+  print(String(format: "VOLUME = %.3f", sphereVolume))
+} else {
+  print("Entrada inválida")
+}
