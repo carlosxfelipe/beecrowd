907bb9f85892f202dd2cecb7eed438ad32daf52e
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-01-24 15:52:17 -0300

    Add initial Swift exercises for basic input/output and calculations

---
+++ swift/iniciante/1009.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+if let inputA = readLine(),
+  let inputB = readLine(),
+  let inputC = readLine(),
+  let fixedSalary = Double(inputB),
+  let totalSales = Double(inputC)
+{
+
+  let sellerName = inputA
+  _ = sellerName  // Ignora o aviso de que 'sellerName' não foi usada
+  let commissionRate = 0.15
+  let commission = totalSales * commissionRate
+  let totalToReceive = fixedSalary + commission
+
+  print(String(format: "TOTAL = R$ %.2f", totalToReceive))
+}
